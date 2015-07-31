7244b9b172e51cf5af9760278ebd9d41dfadc6ac
sgillet1007 <samuelgillet@hotmail.com>
2015-07-31 17:02:27 -0600

    repeat countries prevented

--- public/main.js
+++ public/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
+	var countriesLoaded = false;
 	$('#cntry-btn').on('submit',function(event){
 		event.preventDefault();
 		$.ajax({
 			method : 'GET',
 			url    : '/countries',
 			success : function(data){
-				data.forEach(function(country){
-					// console.log(country.name);
-					$('.cntry-list').append('<li>' + country.name + '</li>');
-					// $('body').after('<li>' + country.name + '</li>');
-				})
+				if (!countriesLoaded){
+					data.forEach(function(country){
+					$('.cntry-list').append('<li class="'+ country.name.replace(/ /g,'') +'">' + country.name + '</li>');
+					countriesLoaded = 1;
+					})
+				}	
+				else {
+					console.log('countriesLoaded:', countriesLoaded);
+				}
 			}
 		})
 	});
@@ -22,11 +27,11 @@ $(document).ready(function(){
 			url      : '/search',
 			success  : function(data){
 				data.forEach(function(country){
-					if ($('#str').val().toLowerCase() === country.name.toLowerCase()) {
+					if ($('#str').val().toLowerCase() === country.name.toLowerCase()){
 						$('.cntry-list').append('<li>' + country.name + '</li>');
 					}
 				})
-			}
+			},
 		})
 	});
 })
